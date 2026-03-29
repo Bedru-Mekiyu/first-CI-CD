b9eeb186e25f632e8457c4bc0ee40871f21f2a19
bedru mekiyu <bedru.mekiyu-ug@aau.edu.et>
2026-03-30 02:06:31 +0300

    adding some security measure

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const helmet = require("helmet");
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 const app = express();
 require("dotenv").config();
+
+app.use(helmet());
+
+app.use(cors({
+  origin: "*", // later restrict this
+}));
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 min
+  max: 100, // limit requests
+});
+
+app.use(limiter);
 // connect to Mongo
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB Connected"))
